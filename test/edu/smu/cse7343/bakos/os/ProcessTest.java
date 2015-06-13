bc1324bd9dd1fca7cfff657fafb7e220a8ff0265
Yong Bakos <ybakos@humanoriented.com>
2015-06-13 21:14:40 +0200

    Add test target to build config. Add temporary ProcessTest class.

---
+++ test/edu/smu/cse7343/bakos/os/ProcessTest.java
@@ -0,0 +1,13 @@
+package edu.smu.cse7343.bakos.os;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ProcessTest {
+
+    @Test
+    public void testFailure() {
+        fail("TODO: Implement ProcessTest tests");
+    }
+    
+}
