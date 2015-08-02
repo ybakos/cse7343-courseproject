8679076e4b6fa32af4b90923f66f6b01c8b45575
Yong Bakos <ybakos@humanoriented.com>
2015-08-02 15:01:01 -0500

    Draw pcbs in the queues.

--- src/edu/smu/cse7343/bakos/os/ProcessQueueView.java
+++ src/edu/smu/cse7343/bakos/os/ProcessQueueView.java
@@ -47,49 +47,21 @@ public class ProcessQueueView {
         p.line(0, 0, 500, 0);
         float sizeOffset = 0.0f;
         for (ProcessControlBlock pcb : queue.queue) {
-            p.println("PCB reg 0: " + pcb.registers[0]);
             p.fill(Float.floatToRawIntBits(pcb.registers[0]));
             p.ellipse(sizeOffset + (pcb.size() / 2.0f), 0, pcb.size(), pcb.size());
             sizeOffset += pcb.size();
         }
         p.popMatrix();
         p.popStyle();
-        // for (ProcessControlBlock pcb : queue.queue) {
-        //     draw the pcbView in its position
-        //     draw the pcbView being executed by the CPU
-        //     p.pushStyle();
-        //     p.pushMatrix();
-        //     p.translate(location.x, location.y);
-        //     p.fill(color);
-        //     p.ellipse(0, 0, size, size);
-        //     p.popMatrix();
-        //     p.popStyle();
-        // }
+        for (ProcessControlBlock pcb : queue.queue) {
+            p.pushStyle();
+            p.pushMatrix();
+            p.translate(pcb.registers[5], pcb.registers[6]);
+            p.fill(Float.floatToRawIntBits(pcb.registers[0]));
+            p.ellipse(0, 0, pcb.size(), pcb.size());
+            p.popMatrix();
+            p.popStyle();
+        }
     }
-
-    // public void draw() {
-    //     for (ProcessView view : processViews.values()) {
-    //         view.draw();
-    //     }
-    //     p.pushStyle();
-    //     p.pushMatrix();
-    //     p.translate(x, y);
-    //     p.stroke(150);
-    //     p.textSize(32);
-    //     p.text(title, 250, -50);
-    //     p.textSize(18);
-    //     p.text("Head", -50, 5);
-    //     p.line(0, 0, 500, 0);
-    //     float sizeOffset = 0.0f;
-    //     for (ProcessControlBlock pcb : queue.queue) {
-    //         ProcessView view = processViews.get(new Integer(pcb.pid));
-    //         view.draw();
-    //         p.fill(view.color);
-    //         p.ellipse(sizeOffset + (view.size / 2.0f), 0, view.size, view.size);
-    //         sizeOffset += (view.size / 2.0f);
-    //     }
-    //     p.popMatrix();
-    //     p.popStyle();
-    // }
 
 }
